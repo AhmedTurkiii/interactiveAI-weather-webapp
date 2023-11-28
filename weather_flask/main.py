d1be3d5a5c65ed8a8ba5116b8e008116a6b9e525
aleguzmancs9 <aleguzmancs9@gmail.com>
2023-11-28 09:28:37 -0800

    Remove unnecessary inputs

--- weather_flask/main.py
+++ weather_flask/main.py
@@ -1,6 +1,5 @@
 # main.py
 import requests
-from datetime import datetime
 
 # Read the API key for the weather service from a file named 'api_key'
 API_KEY = open('api_key_weatherAPI', 'r').read()
@@ -9,7 +8,6 @@ API_KEY = open('api_key_weatherAPI', 'r').read()
 def get_weather_data(city):
     # Construct the URL for the weather API request
     url = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city}&days=1&aqi=no&alerts=no'
-    # Make an HTTP GET request to the weather API
     response = requests.get(url)
     # Check if the response status code is 200 (OK)
     if response.status_code == 200:
@@ -19,5 +17,4 @@ def get_weather_data(city):
     else:
         # In case of a non-200 response, print an error message
         print('Error fetching weather data')
-        # Optionally, return None or handle the error as needed
-        # return None
+
